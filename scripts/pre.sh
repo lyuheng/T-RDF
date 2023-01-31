d5f34a49edf285e6f8eb79755973e9bfbfe20b3f
lyuheng <46214553+lyuheng@users.noreply.github.com>
2023-01-30 18:04:28 -0600

    Create pre.sh

---
+++ scripts/pre.sh
@@ -0,0 +1,7 @@
+rm -rf tools/libantlr3c-3.4/
+cd tools; tar -zxvf libantlr3c-3.4.tar.gz
+cd libantlr3c-3.4/;  ./configure --enable-64bit; make; cd ../../
+rm -rf lib/libantlr.a
+ar -crv lib/libantlr.a tools/libantlr3c-3.4/*.o 
+rm -rf Parser/Sparql*
+cd tools; tar -xzvf sparql.tar.gz; mv Sparql* ../Parser/
